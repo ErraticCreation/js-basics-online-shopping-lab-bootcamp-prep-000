e6b4aff223674d1528b3ab6710f7b0f233562be8
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-17 02:30:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function placeOrder(cardNumber) {
   if(!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    let message = `Your total cost is ${total}, which will be charged to your card ${cardNumber}.`;
+    let message = `Your total cost is ${total()}, which will be charged to your card ${cardNumber}.`;
     cart = [];
     return message;
   }
